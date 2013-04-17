2ab55b3b2e81fb7855b751555ba63ccabf6ec67f
chassing <chris@ca-net.org>
2013-04-17 17:56:41 +0200

    rename s3 static dir

--- reversi/s3storages.py
+++ reversi/s3storages.py
@@ -3,7 +3,7 @@ from storages.backends.s3boto import S3BotoStorage
 # Define bucket and folder for static files.
 StaticStorage = lambda: S3BotoStorage(
     bucket='se2reversi',
-    location='assets')
+    location='static')
 
 
 # Define bucket and folder for media files.
